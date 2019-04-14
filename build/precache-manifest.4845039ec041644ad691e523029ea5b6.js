a5a962b2f1f7111368a0d980060429a0830338ce
lrush85 <lance.rush1@gmail.com>
2019-04-14 13:16:12 -0500

    fixed error from deployment to gh-pages

---
+++ build/precache-manifest.4845039ec041644ad691e523029ea5b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5774ddf91251bd4ee9c2",
+    "url": "/clicky-game/static/css/main.9e358697.chunk.css"
+  },
+  {
+    "revision": "5774ddf91251bd4ee9c2",
+    "url": "/clicky-game/static/js/main.16797fb5.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "a78fb2f6e4e0478b8c1d96dd0d665038",
+    "url": "/clicky-game/index.html"
+  }
+];
