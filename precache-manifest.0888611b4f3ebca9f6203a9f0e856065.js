6e9ed64c3043b55ddc9f33663f67939e43c25c1c
lrush85 <lance.rush1@gmail.com>
2019-04-14 13:17:42 -0500

    Updates

---
+++ precache-manifest.0888611b4f3ebca9f6203a9f0e856065.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5774ddf91251bd4ee9c2",
+    "url": "/clicky-game/static/css/main.9e358697.chunk.css"
+  },
+  {
+    "revision": "5774ddf91251bd4ee9c2",
+    "url": "/clicky-game/static/js/main.16797fb5.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "fafb46a1a7d38d3274ae37a9eb5d18cb",
+    "url": "/clicky-game/index.html"
+  }
+];
